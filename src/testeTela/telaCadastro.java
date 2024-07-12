0df8d4310baf7394d65c2fb911b3f032b84ef45f
João Matheus <matheusbarbosa.or@gmail.com>
2024-07-11 22:27:24 -0300

    Nova tela de Cadastro & Undo Rename

---
+++ src/testeTela/telaCadastro.java
@@ -0,0 +1,17 @@
+package testeTela;
+
+import javax.swing.*;
+
+public class telaCadastro {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JRadioButton femininoRadioButton;
+    private JRadioButton masculinoRadioButton;
+    private JRadioButton outrosRadioButton;
+    private JTextField textField6;
+}
